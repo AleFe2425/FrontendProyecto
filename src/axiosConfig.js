c01664a1de8a7f7db2649ccbda9928733bd13264
AleFe2425 <ignacio.fonte@udla.edu.ec>
2024-11-09 21:25:59 -0500

    Configuracion del axios

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080', // URL base de tu backend
+    baseURL: 'http://refreshing-expression-production.up.railway.app', // URL base de tu backend
     timeout: 10000, // Opcional: tiempo de espera para las solicitudes
 });
 
